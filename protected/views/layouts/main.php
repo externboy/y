2ca68b16e32127e6eb560e9145173cb7b442c787
viki <viki.b@outwitinc.com>
2015-01-22 11:24:26 +0800

    before

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -15,6 +15,7 @@
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css">
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css">
 
+	<script src="http://libs.baidu.com/jquery/1.9.0/jquery.js"></script>
 	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
 </head>
 
